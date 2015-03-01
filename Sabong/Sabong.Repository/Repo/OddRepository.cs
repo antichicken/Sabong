29a78217eb398147dd9d3b707d7187fea566bc3f
antichicken <ge.binhbd@gmail.com>
2015-03-01 11:09:52 +0700

    Edit: Jump Odd work flow--> update date vao dictionary + db. Thêm Update OddDiffCal trong repo

--- Sabong/Sabong.Repository/Repo/OddRepository.cs
+++ Sabong/Sabong.Repository/Repo/OddRepository.cs
@@ -1,4 +1,5 @@
-﻿using Sabong.Repository.EntityModel;
+﻿using System.Data;
+using Sabong.Repository.EntityModel;
 
 namespace Sabong.Repository.Repo
 {
@@ -9,5 +10,22 @@ namespace Sabong.Repository.Repo
             var xxx = new oddsdiff_calc();
             return xxx;
         }
+
+        public void UpdateOddDiffCalc(oddsdiff_calc oddsdiff)
+        {
+            using (s_dbEntities context = new s_dbEntities())
+            {
+                context.oddsdiff_calc.Attach(oddsdiff);
+
+                var entry = context.Entry(oddsdiff);
+                entry.State = EntityState.Modified;
+
+                entry.Property(e => e.diff).IsModified = false;
+                entry.Property(e => e.match_slno).IsModified = false;
+
+                context.SaveChanges();
+            }
+        }
+
     }
 }
