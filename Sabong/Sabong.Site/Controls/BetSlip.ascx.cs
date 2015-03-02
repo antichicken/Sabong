45f95c966ae23ba776ba5c8043fa47eb95d46a26
tung1608 <tungkhac.kk@gmail.com>
2015-03-02 22:36:07 +0700

    style + node + match info

--- Sabong/Sabong.Site/Controls/BetSlip.ascx.cs
+++ Sabong/Sabong.Site/Controls/BetSlip.ascx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Sabong.Business;
 
 public partial class Controls_BetSlip : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    public MatchStatus Status
+    {
+        get; set;
     }
 }
