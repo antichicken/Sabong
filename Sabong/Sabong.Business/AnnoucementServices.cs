173f4da73695a52a94a90a0d9c1211a35c7e7ddd
antichicken <ge.binhbd@gmail.com>
2015-03-02 21:59:59 +0700

    addd annoucement service

---
+++ Sabong/Sabong.Business/AnnoucementServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sabong.Business.BO;
+using Sabong.Repository.Repo;
+
+namespace Sabong.Business
+{
+   public  class AnnoucementServices
+    {
+       public List<AnnoucementBO> GetAll()
+       {
+           List<AnnoucementBO> retval = new List<AnnoucementBO>();
+           Repository.Repo.AnnoucementRepository repo=new AnnoucementRepository();
+           var annouce=repo.GetAll();
+           foreach (var announcement in annouce)
+           {
+               AnnoucementBO addValue=new AnnoucementBO();
+               addValue.id = announcement.slno;
+               addValue.Date = announcement.date.ToString("dd-MM-yyyy");
+               addValue.Subject = announcement.subject;
+
+               addValue.ArenaName = repo.GetArenaNameByDate(announcement.date.ToString("dd-MM-yyyy"));
+
+               retval.Add(addValue);
+           }
+           return retval;
+       }
+    }
+}
--- Sabong/Sabong.Repository/Repo/AnnoucementRepository.cs
+++ Sabong/Sabong.Repository/Repo/AnnoucementRepository.cs
@@ -2,7 +2,8 @@
 ﻿
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data;
+﻿using System.Linq;
 
 
 
@@ -13,19 +14,36 @@ namespace Sabong.Repository.Repo
     public class AnnoucementRepository
     {
         //select * from announcement WHERE (date >= DATE_SUB(CURDATE(), INTERVAL 9 DAY)) order by date DESC,slno desc
+
+        //select arena_name from arena,fight_assign where fight_assign.date='$dt' and fight_assign.arena=arena.id and fight_assign.`cancelmatch`!=1 order by fight_assign.slno desc
+        public string GetArenaNameByDate(string date)
+        {
+          //  string datestring = date.ToString("dd-MM-yyyy");
+            using (s_dbEntities context = new s_dbEntities())
+            {
+                var xxx= (from are in context.arenas
+                    join fight in context.fight_assign on are.id equals fight.arena
+                          where fight.date == date &&
+                          fight.cancelmatch != 1
+                    orderby fight.slno descending
+                    select are.arena_name).FirstOrDefault();
+                return xxx;
+            }
+        }
+
         public List<announcement> GetAll()
         {
             var currData = DateTime.Now.AddDays(-9);
             using (s_dbEntities context = new s_dbEntities())
             {
-                context.Database.Connection.Open();
+              //  context.Database.Connection.Open();
                 var result = from announcement in context.announcements
                     where announcement.date>= currData
                     orderby announcement.date descending 
 
                     select announcement;
                 //  return result.ToList();
-                context.Database.Connection.Close();
+              //  context.Database.Connection.Close();
                 return result.ToList();
 
                 
