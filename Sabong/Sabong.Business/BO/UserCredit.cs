ff7a6179527707c1d4bee7e62bb41aa7a20a0a2c
tung1608 <tungkhac.kk@gmail.com>
2015-03-08 00:51:15 +0700

    merge code

---
+++ Sabong/Sabong.Business/BO/UserCredit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabong.Business.BO
+{
+    public class UserCredit
+    {
+        public double GivenCredit { get; set; }
+        public double Profit { get; set; }
+        public double BetCredit { get; set; }
+    }
+}
